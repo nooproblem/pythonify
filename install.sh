e3fbe264ee0fd92775cb087bb4119711930e3588
np <np@air.home>
2023-05-15 20:52:02 +0100

    .gitignore

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 curl -s https://raw.githubusercontent.com/nooproblem/pythonify/master/flake.nix > flake.nix
 echo "use flake" > ./.envrc
 direnv allow
+curl -s https://raw.githubusercontent.com/nooproblem/pythonify/master/.gitignore >> .gitignore
